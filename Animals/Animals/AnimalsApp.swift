6c8b14780b1217f32decadab967a7e74f4c4ebdc
yevhenii.stepanov <yevhenii.stepanov@stfalcon.comyevhenii.stepanov@stfalcon.comyevhenii.stepanov@stfalcon.com>
2025-10-02 15:26:56 +0300

    create project

---
+++ Animals/Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Yevhenii Stepanov on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animals/Animals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animals
+//
+//  Created by Yevhenii Stepanov on 02.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
